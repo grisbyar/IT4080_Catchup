74945952005e40ebbddcd9c563eb9281b4cde14a
grisbyar <64669576+grisbyar@users.noreply.github.com>
2023-10-02 23:16:55 -0400

    Additional commit

--- Assets/Arena1Game.cs
+++ Assets/Arena1Game.cs
@@ -1,54 +1,63 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class Arena1Game : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-
-//      private int positionIndex = 0;
-//     private Vector3[] startPositions = new Vector3[]
-//     {
-//         new Vector3(4, 2, 0),
-//         new Vector3(-4, 2, 0),
-//         new Vector3(0, 2, 4),
-//         new Vector3(0, 2, -4)
-//     };
-
-//     private int colorIndex = 0;
-//     private Color[] playerColors = new Color[] {
-//         Color.blue,
-//         Color.green,
-//         Color.yellow,
-//         Color.magenta,
-//     };
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
 
 
-//     private Vector3 NextPosition() {
-//         Vector3 pos = startPositions[positionIndex];
-//         positionIndex += 1;
-//         if (positionIndex > startPositions.Length - 1) {
-//             positionIndex = 0;
-//         }
-//         return pos;
-//     }
+public class Arena1Game : NetworkBehaviour
+{
+    public Player playerPrefab;
+    public Camera arenaCamera;
 
+    private int positionIndex = 0;
+    private Vector3[] startPositions = new Vector3[]
+    {
+        new Vector3(4, 2, 0),
+        new Vector3(-4, 2, 0),
+        new Vector3(0, 2, 4),
+        new Vector3(0, 2, -4)
+    };
 
-//     private Color NextColor() {
-//         Color newColor = playerColors[colorIndex];
-//         colorIndex += 1;
-//         if (colorIndex > playerColors.Length - 1) {
-//             colorIndex = 0;
-//         }
-//         return newColor;
-//     }
-//     }
-// }
+    private int colorIndex = 0;
+    private Color[] playerColors = new Color[] {
+        Color.blue,
+        Color.green,
+        Color.yellow,
+        Color.magenta,
+    };
+
+    private Color NextColor() {
+        Color newColor = playerColors[colorIndex];
+        colorIndex += 1;
+        if (colorIndex > playerColors.Length - 1) {
+            colorIndex = 0;
+        }
+        return newColor;
+    }
+
+    void Start()
+    {
+        arenaCamera.enabled = !IsClient;
+        arenaCamera.GetComponent<AudioListener>().enabled = !IsClient;
+        if(IsServer){
+            SpawnPlayers();
+        }
+    }
+
+    private Vector3 NextPosition() {
+        Vector3 pos = startPositions[positionIndex];
+        positionIndex += 1;
+        if (positionIndex > startPositions.Length - 1) {
+            positionIndex = 0;
+        }
+        return pos;
+    }
+    
+    private void SpawnPlayers(){
+        foreach(ulong clientId in NetworkManager.ConnectedClientsIds)
+        {
+            Player playerSpawn = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+            playerSpawn.GetComponent<NetworkObject>().SpawnWithOwnership(clientId);
+        }
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,48 +1,68 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
 
-// public class Player : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
+public class Player : NetworkBehaviour
+{
+    public float movementSpeed = 50f;
+    public float rotationSpeed = 130f;
+
+    private Camera playerCamera;
+    private void Start(){
+        playerCamera = transform.Find("Camera").GetComponent<Camera>();
+        playerCamera.enabled = IsOwner;
+        playerCamera.GetComponent<AudioListener>().enabled = IsOwner;
+    }
+    // Rotate around the y axis when shift is not pressed
+    private Vector3 CalcRotation() {
+        bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        Vector3 rotVect = Vector3.zero;
+        if (!isShiftKeyDown) {
+            rotVect = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+            rotVect *= rotationSpeed * Time.deltaTime;
+        }
+        return rotVect;
+    }
+
+
+    // Move up and back, and strafe when shift is pressed
+    private Vector3 CalcMovement() {
+        bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        float x_move = 0.0f;
+        float z_move = Input.GetAxis("Vertical");
+
+        if (isShiftKeyDown) {
+            x_move = Input.GetAxis("Horizontal");
+        }
+
+        Vector3 moveVect = new Vector3(x_move, 0, z_move);
+        moveVect *= movementSpeed * Time.deltaTime;
+
+        return moveVect;
+    }
+
+    private void OwnerHandleInput()
+    {
+        Vector3 movement = CalcMovement();
+        Vector3 rotation = CalcRotation();
+        if(movement != Vector3.zero || rotation != Vector3.zero){
+            MoveServerRpc(CalcMovement(), CalcRotation());
+        }
         
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//     public float movementSpeed = 50f;
-//     public float rotationSpeed = 130f;
-
-//     // Rotate around the y axis when shift is not pressed
-//     private Vector3 CalcRotation() {
-//         bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-//         Vector3 rotVect = Vector3.zero;
-//         if (!isShiftKeyDown) {
-//             rotVect = new Vector3(0, Input.GetAxis("Horizontal"), 0);
-//             rotVect *= rotationSpeed * Time.deltaTime;
-//         }
-//         return rotVect;
-//     }
-
-
-//     // Move up and back, and strafe when shift is pressed
-//     private Vector3 CalcMovement() {
-//         bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-//         float x_move = 0.0f;
-//         float z_move = Input.GetAxis("Vertical");
-
-//         if (isShiftKeyDown) {
-//             x_move = Input.GetAxis("Horizontal");
-//         }
-
-//         Vector3 moveVect = new Vector3(x_move, 0, z_move);
-//         moveVect *= movementSpeed * Time.deltaTime;
-
-//         return moveVect;
-//     }
-
-//     }
-// }
+    }
+ 
+    [ServerRpc]
+    private void MoveServerRpc(Vector3 movement, Vector3 rotation){
+        transform.Translate(movement);
+        transform.Rotate(rotation);
+    }
+  
+    private void Update()
+    {
+        if(IsOwner){
+            OwnerHandleInput();
+        }
+        
+    }
+}
